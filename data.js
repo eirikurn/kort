37f706d1a3046a586d160776db0a9b285a19a666
Eirikur Nilsson <eirikur@nilsson.is>
2012-08-06 03:25:00 +0000

    Initial commit. Map with orkan stations.

---
+++ data.js
@@ -0,0 +1,16 @@
+var data = [
+  { x: 355281, y: 399013, category: "Gas", chain: "Orkan" },
+  { x: 356982, y: 400486, category: "Gas", chain: "Orkan" },
+  { x: 360274, y: 403413, category: "Gas", chain: "Orkan" },
+  { x: 361102, y: 404016, category: "Gas", chain: "Orkan" },
+  { x: 363740, y: 404661, category: "Gas", chain: "Orkan" },
+  { x: 357991, y: 406501, category: "Gas", chain: "Orkan" },
+  { x: 359146, y: 406538, category: "Gas", chain: "Orkan" },
+  { x: 359140, y: 406455, category: "Gas", chain: "Orkan" },
+  { x: 357987, y: 406502, category: "Gas", chain: "Orkan" },
+  { x: 360587, y: 408772, category: "Gas", chain: "Orkan" },
+  { x: 354817, y: 408608, category: "Gas", chain: "Orkan" },
+  { x: 354779, y: 408827, category: "Gas", chain: "Orkan" },
+  { x: 363541, y: 407667, category: "Gas", chain: "Orkan" },
+  { x: 364384, y: 408212, category: "Gas", chain: "Orkan" }
+]
